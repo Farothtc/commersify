10887efd2b972d4082ab0f9b3b270330b138b6b3
Farothtc <mvkirimi@gmail.com>
2025-06-20 19:44:15 +0300

    feat: general structure improved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import NavUp from "./components/NavUp";
+import NavDown from "./components/NavDown";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <section>
+      <section className="container mx-auto">
+        <div className="bg-[#e2e7ff]">
+          <NavUp />
+        </div>
+        <NavDown />
         <h1>Halo</h1>
       </section>
     </main>
