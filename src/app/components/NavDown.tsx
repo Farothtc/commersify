c0e5057366622f46bb71001f90fa9fd72019897a
Farothtc <mvkirimi@gmail.com>
2025-06-23 15:14:38 +0300

    feat: structural improvements made

--- src/app/components/NavDown.tsx
+++ src/app/components/NavDown.tsx
@@ -1,21 +1,40 @@
 import navData from "../lib/navData";
 import Image from "next/image";
+import Link from "next/link";
 export default function NavDown() {
   return (
     <nav className="flex justify-between items-center gap-0">
       {navData.map((e) => (
         <div
           key={e.id}
-          className="flex items-center gap-1 hover:bg-[#e2e7ff] mt-1 w-36 rounded-2xl"
+          className="flex justify-between items-center gap-1 mt-1 w-40 rounded-2xl"
         >
-          <Image
-            src={e.img}
-            alt={e.title}
-            width={70}
-            height={70}
-            className="p-2 rounded-bl-2xl rounded-xl"
-          ></Image>
-          <h1 className="text-black text-xs">{e.title}</h1>
+          <div className="dropdown dropdown-hover">
+            <Link href={`${"/" + e.url}`}>
+              <div
+                tabIndex={e.id}
+                role="button"
+                className="btn btn-ghost shadow-none text-xs hover:bg-[#e2e7ff] border-0 w-full flex justify-between items-center"
+              >
+                <Image
+                  src={e.img}
+                  alt={e.title}
+                  width={55}
+                  height={55}
+                  className="p-2 rounded-bl-2xl rounded-xl"
+                ></Image>
+                <h1 className="text-black text-xs">{e.title}</h1>
+              </div>
+            </Link>
+            <div
+              tabIndex={e.id}
+              className="dropdown-content card card-md bg-white z-1 w-98 mt-0.5 shadow-md"
+            >
+              <div className="card-body">
+                <p>This is a card. You can use any element as a dropdown.</p>
+              </div>
+            </div>
+          </div>
         </div>
       ))}
     </nav>
