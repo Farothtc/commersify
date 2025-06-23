cea0fed3a898d8ee88f9a6902efc5b96bbbe8dcb
Farothtc <mvkirimi@gmail.com>
2025-06-23 15:14:26 +0300

    feat: new type and data introduced

--- src/app/lib/navData.ts
+++ src/app/lib/navData.ts
@@ -2,6 +2,7 @@ type NavData = {
   id: number;
   img: string;
   title: string;
+  url: string;
 };
 
 const navData = [
@@ -9,46 +10,55 @@ const navData = [
     id: 1,
     img: "/NavDown/fashion.jpg",
     title: "Fashion",
+    url: "fashion",
   },
   {
     id: 2,
     img: "/NavDown/electronics.jpg",
     title: "Electronics",
+    url: "electronics",
   },
   {
     id: 3,
     img: "/NavDown/home.jpg",
     title: "Home & Life",
+    url: "home-life",
   },
   {
     id: 4,
     img: "/NavDown/fashion.jpg",
     title: "Fashion",
+    url: "fashion",
   },
   {
     id: 5,
     img: "/NavDown/cosmetics.jpg",
     title: "Cosmetics & Personal Care",
+    url: "cosmetics-care",
   },
   {
     id: 6,
     img: "/NavDown/jewelry.jpg",
     title: "Jewelry & Watch",
+    url: "jewelry-watch",
   },
   {
     id: 7,
     img: "/NavDown/fashion.jpg",
     title: "Fashion",
+    url: "fashion",
   },
   {
     id: 8,
     img: "/NavDown/fashion.jpg",
     title: "Fashion",
+    url: "fashion",
   },
   {
     id: 9,
     img: "/NavDown/fashion.jpg",
     title: "Fashion",
+    url: "fashion",
   },
 ];
 
