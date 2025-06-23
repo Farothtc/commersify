5801167cc3a0318a50a5e0e880803d0804950a27
Farothtc <mvkirimi@gmail.com>
2025-06-23 13:45:31 +0300

    feat: navbar down area structure improved

--- src/app/components/NavDown.tsx
+++ src/app/components/NavDown.tsx
@@ -1,7 +1,23 @@
+import navData from "../lib/navData";
+import Image from "next/image";
 export default function NavDown() {
   return (
-    <nav>
-      <h1>Halo navdown</h1>
+    <nav className="flex justify-between items-center gap-0">
+      {navData.map((e) => (
+        <div
+          key={e.id}
+          className="flex items-center gap-1 hover:bg-[#e2e7ff] mt-1 w-36 rounded-2xl"
+        >
+          <Image
+            src={e.img}
+            alt={e.title}
+            width={70}
+            height={70}
+            className="p-2 rounded-bl-2xl rounded-xl"
+          ></Image>
+          <h1 className="text-black text-xs">{e.title}</h1>
+        </div>
+      ))}
     </nav>
   );
 }
