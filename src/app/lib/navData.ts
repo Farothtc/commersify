77e0e1b7d2884a3b93a11e952cdb7f80baeba78d
Farothtc <mvkirimi@gmail.com>
2025-06-23 13:45:38 +0300

    feat: new data added

--- src/app/lib/navData.ts
+++ src/app/lib/navData.ts
@@ -7,7 +7,47 @@ type NavData = {
 const navData = [
   {
     id: 1,
-    img: "/NavDown/",
+    img: "/NavDown/fashion.jpg",
+    title: "Fashion",
+  },
+  {
+    id: 2,
+    img: "/NavDown/electronics.jpg",
+    title: "Electronics",
+  },
+  {
+    id: 3,
+    img: "/NavDown/home.jpg",
+    title: "Home & Life",
+  },
+  {
+    id: 4,
+    img: "/NavDown/fashion.jpg",
+    title: "Fashion",
+  },
+  {
+    id: 5,
+    img: "/NavDown/cosmetics.jpg",
+    title: "Cosmetics & Personal Care",
+  },
+  {
+    id: 6,
+    img: "/NavDown/jewelry.jpg",
+    title: "Jewelry & Watch",
+  },
+  {
+    id: 7,
+    img: "/NavDown/fashion.jpg",
+    title: "Fashion",
+  },
+  {
+    id: 8,
+    img: "/NavDown/fashion.jpg",
+    title: "Fashion",
+  },
+  {
+    id: 9,
+    img: "/NavDown/fashion.jpg",
     title: "Fashion",
   },
 ];
